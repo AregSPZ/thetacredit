6d47f73453d9195b29810cb73566d7dc62eb04a2
Areg Sargsyan <quant0027@gmail.com>
2024-09-17 19:04:37 +0400

    solve secret key error

--- app/config.py
+++ app/config.py
@@ -6,3 +6,6 @@ load_dotenv() # take environment variables from .env file
 class Config:
 
     SECRET_KEY = os.getenv('SECRET_KEY')
+    
+    if not SECRET_KEY:
+        raise RuntimeError("A secret key is required to use CSRF.")
